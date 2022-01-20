7abb5c12679fd60d8a1f73cfc71e5341e862ef21
Khalid Safy <khalid_safy88@hotmail.com>
2022-01-20 22:45:14 +0000

    changes before checkout

--- frontend/src/components/InspirationArticles.js
+++ frontend/src/components/InspirationArticles.js
@@ -12,29 +12,39 @@ const InspirationArticles = () => {
       <h2>Need help picking a destination? Get inspiration!</h2>
       <div className="inspiration-articles">
         <div className="backpacking-container">
-          <img src={backpacking} alt="backpacking" />
+          <a href="https://www.bucketlistly.blog/posts/best-backpacking-adventure-blogs">
+            <img src={backpacking} alt="backpacking" />
+          </a>
           <h3 className="backpacking-text">
             10 Best Adventure & Backpacking Blogs to Follow in 2022
           </h3>
         </div>
         <div className="cheapest-container">
-          <img src={cheapest} alt="cheapest" />
+          <a href="https://www.bucketlistly.blog/posts/cheap-countries-to-visit">
+            <img src={cheapest} alt="cheapest" />
+          </a>
           <h3 className="cheapest-text">
             10 Cheapest Countries to Visit in 2022
           </h3>
         </div>
         <div className="vacc-container">
-          <img src={vacc} alt="vaccine safe" />
+          <a href="https://www.bucketlistly.blog/posts/countries-open-to-vaccinated-travelers">
+            <img src={vacc} alt="vaccine safe" />
+          </a>
           <h3 className="vacc-text">
             27 Countries That Are Open to Vaccinated Travelers
           </h3>
         </div>
         <div className="getlost-container">
-          <img src={getlost} alt="how to get lost article" />
+          <a href="https://www.bucketlistly.blog/posts/how-to-get-lost-more-comfortably-while-traveling">
+            <img src={getlost} alt="how to get lost article" />
+          </a>
           <h3 className="getlost-text">How to Get Lost...</h3>
         </div>
         <div className="bestbooks-container">
-          <img src={bestbooks} alt="best travel books article" />
+          <a href="https://www.bucketlistly.blog/posts/best-travel-adventure-books">
+            <img src={bestbooks} alt="best travel books article" />
+          </a>
           <h3 className="bestbooks-text">
             8 Great Travel Books to Read in 2022
           </h3>
